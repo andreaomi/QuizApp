69c3786d49e1562aeda52ba5b5984cd1229c331c
Andrea Omicevic <andrea.omicevic@fer.hr>
2020-05-09 22:18:07 +0200

    Initial Commit

---
+++ QuizApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by Andrea Omićević on 09/05/2020.
+//  Copyright © 2020 Andrea Omićević. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
